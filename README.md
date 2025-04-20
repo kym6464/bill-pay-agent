723b86bebb8e71de5c74e5dbe949f50b0915bb72
Kamal Muradov <muradovkamal10@gmail.com>
2025-04-20 16:18:11 -0400

    add demo link

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # bill-pay
 
 The goal is to have an AI navigate to lasa.org, login, and pay the monthly bill.
+
+Demo: [link](https://storage.googleapis.com/kamal-screenshots/385f5f528193614a8b6d7aa1e2acbeb0.html)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
+import asyncio
+import os
+
 from langchain_openai import ChatOpenAI
 from browser_use import Agent
-import asyncio
 from dotenv import load_dotenv
+
 load_dotenv()
+sensitive_data = {
+    "LASA_USERNAME": os.environ["LASA_USERNAME"],
+    "LASA_PASSWORD": os.environ["LASA_PASSWORD"],
+}
+
 
 async def main():
     agent = Agent(
-        task="Compare the price of gpt-4o and DeepSeek-V3",
+        task="Go to lasa.org and login to the online payment portal using LASA_USERNAME and LASA_PASSWORD. Then proceed to make a payment using the existing payment method.",
         llm=ChatOpenAI(model="gpt-4o"),
+        sensitive_data=sensitive_data,
     )
     await agent.run()
 
-asyncio.run(main())
 
+asyncio.run(main())
